dd4f2d7d2981ff676863364c506f94c9cd63fa46
Austin Hibdon <austin.hibdon@gmail.com>
2015-08-27 13:25:20 -0400

    oops

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ use \Dropbox as dbx;
 $appInfo = dbx\AppInfo::loadFromJsonFile("app-info.json");
 $accessToken = "PR7lZnGybKkAAAAAAAAXYa_wAj7RAns-RD6uHMTbsRP4BmKsgNtjeSUqrgc8BHJ6";
 
+$dbxClient = new dbx\Client($accessToken, "PHP-Example/1.0");
+
 $f = fopen("app-info.json", "rb");
 $result = $dbxClient->uploadFile("/working-draft.txt", dbx\WriteMode::add(), $f);
 fclose($f);

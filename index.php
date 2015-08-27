bfbc0566fb19774baa68d148c596f6302a161b46
Austin Hibdon <austin.hibdon@gmail.com>
2015-08-27 13:21:07 -0400

    I got that access token, now what...?

--- index.php
+++ index.php
@@ -2,11 +2,8 @@
 require_once('lib/Dropbox/autoload.php');
 use \Dropbox as dbx;
 $appInfo = dbx\AppInfo::loadFromJsonFile("app-info.json");
-$webAuth = new dbx\WebAuthNoRedirect($appInfo, "PHP-Example/1.0");
-$authorizeUrl = $webAuth->start();
-echo "1. Go to: " . $authorizeUrl . "\n";
-echo "2. Click \"Allow\" (you might have to log in first).\n";
-echo "3. Copy the authorization code.\n";
-$authCode = \trim(\readline("Enter the authorization code here: "));
-list($accessToken, $dropboxUserId) = $webAuth->finish($authCode);
-print "Access Token: " . $accessToken . "\n";
+$accessToken = "PR7lZnGybKkAAAAAAAAXYa_wAj7RAns-RD6uHMTbsRP4BmKsgNtjeSUqrgc8BHJ6";
+
+$dbxClient = new dbx\Client($accessToken, "PHP-Example/1.0");
+$accountInfo = $dbxClient->getAccountInfo();
+print_r($accountInfo);

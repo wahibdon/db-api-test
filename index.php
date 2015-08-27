42b6e51b481668b030e53fe93eaa39ad4253e3c4
Austin Hibdon <austin.hibdon@gmail.com>
2015-08-27 13:43:26 -0400

    what happens on error

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $accessToken = "PR7lZnGybKkAAAAAAAAXYa_wAj7RAns-RD6uHMTbsRP4BmKsgNtjeSUqrgc8BHJ6
 
 $dbxClient = new dbx\Client($accessToken, "PHP-Example/1.0");
 
-$f = fopen("app-info.json", "rb");
+$f = fopen("app-info.jso", "rb");
 $result = $dbxClient->uploadFile("/working-draft.txt", dbx\WriteMode::add(), $f);
 fclose($f);
 print_r($result);

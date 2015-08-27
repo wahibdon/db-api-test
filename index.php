91d1d6e5c490490d4cb592e7b3e2517e9e3bb625
Austin Hibdon <austin.hibdon@gmail.com>
2015-08-27 15:25:10 -0400

    emailing seems to work, the link in the body does not for some reason.

--- index.php
+++ index.php
@@ -14,13 +14,12 @@ if ($_FILES['SelectedFile']){
 	$result = $dbxClient->uploadFile("/$file", dbx\WriteMode::add(), $f);
 	fclose($f);
 	print_r($result);
-	$message = "A new file has been uploaded and can be viewed at https://www.dropbox.com/home/Apps/Ott%20Clients/$file .\n
+	$message = "A new file has been uploaded and can be viewed at https://www.dropbox.com/home/Apps/Ott%20Clients{$result['path']} .\n
 	File information:\n
 	Name: {$_POST['first']} {$_POST['last']}\n
 	Email: {$_POST['email']}\n
-	Descrption: {$_POST['description']}
-	";
-	mail('whibdon@ottcom.com', 'New File on Dropbox', $message);
+	Descrption: {$_POST['description']}";
+	mail('whibdon@ottcom.com', 'New File on Dropbox', $message, "From: 'Ottcom Uploads' <noreply@ottcom.com>");
 	die();
 }
 ?>

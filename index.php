57d3445ffc55b568a349faaa730ac226fefd420b
Austin Hibdon <austin.hibdon@gmail.com>
2015-08-27 13:44:37 -0400

    error handling

--- index.php
+++ index.php
@@ -7,6 +7,10 @@ $accessToken = "PR7lZnGybKkAAAAAAAAXYa_wAj7RAns-RD6uHMTbsRP4BmKsgNtjeSUqrgc8BHJ6
 $dbxClient = new dbx\Client($accessToken, "PHP-Example/1.0");
 
 $f = fopen("app-info.jso", "rb");
-$result = $dbxClient->uploadFile("/working-draft.txt", dbx\WriteMode::add(), $f);
+try {
+	$result = $dbxClient->uploadFile("/working-draft.txt", dbx\WriteMode::add(), $f);
+}catch{
+	$result = "failed";
+}
 fclose($f);
 print_r($result);
